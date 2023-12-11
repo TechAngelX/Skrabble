b0ec515beda35f5f4d9c0957f47f25fad86d3b3d
Ricki Angel <rangel03@student.bbk.ac.uk>
2023-12-11 22:55:54 +0000

    Started basic input validation on (l/d)

--- src/pij/main/game_Start.java
+++ src/pij/main/game_Start.java
@@ -3,7 +3,7 @@ package pij.main;
 import java.util.Scanner;
 
 public class game_Start {
-    private char boardType;
+    private String boardType;
 
     public void intro_Header(){
         System.out.println("============                   ============");
@@ -11,16 +11,12 @@ public class game_Start {
         System.out.println("============                   ============");
     }
 
-    public char board_Config(){
+    public String board_Config(){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Would you like to _l_oad a board or use the _d_efault board?\n" +
+        System.out.print("Would you like to _l_oad a board or use the _d_ef board?\n" +
                 "Please enter your choice (l/d): ");
-        String tmp = scanner.nextLine();
-        if (tmp.toString().length() > 1){
-            //Error Handle
-        } else {
-            boardType = tmp.charAt(0);
-        }
+        String boardTyoe = String.valueOf(scanner.nextLine().toLowerCase().toString().charAt(0));
+//        if (!boardType equals '')
         System.out.println("your option is: " + boardType);
         return boardType;
 
