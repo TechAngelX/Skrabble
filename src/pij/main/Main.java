b26dd60421a16d0e224dfc8aaabae1b25f58c73d
Ricki Angel <rangel03@student.bbk.ac.uk>
2023-12-11 22:22:58 +0000

    added game_Start class, intro_header and board_Config methods

--- src/pij/main/Main.java
+++ src/pij/main/Main.java
@@ -1,5 +1,12 @@
 package pij.main;
 
 public class Main {
+    public static void main(String[] args) {
+        game_Start newGame = new game_Start();
+        newGame.intro_Header();
+        newGame.board_Config();
+
+    }
+
 
 }
---
+++ src/pij/main/game_Start.java
@@ -0,0 +1,29 @@
+package pij.main;
+
+import java.util.Scanner;
+
+public class game_Start {
+    private char boardType;
+
+    public void intro_Header(){
+        System.out.println("============                   ============");
+        System.out.println("============ S k r a B B K l e ============");
+        System.out.println("============                   ============");
+    }
+
+    public char board_Config(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Would you like to _l_oad a board or use the _d_efault board?\n" +
+                "Please enter your choice (l/d): ");
+        String tmp = scanner.nextLine();
+        if (tmp.toString().length() > 1){
+            //Error Handle
+        } else {
+            boardType = tmp.charAt(0);
+        }
+        System.out.println("your option is: " + boardType);
+        return boardType;
+
+
+    }
+}
