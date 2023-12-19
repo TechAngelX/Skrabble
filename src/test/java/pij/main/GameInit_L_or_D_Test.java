59dc9d20e3ed5a1552a096f457ce7e4a12753b02
Ricki Angel <rangel03@student.bbk.ac.uk>
2023-12-19 04:19:04 +0000

    More clearing of dir structure, and work on JUnit tests

---
+++ src/test/java/pij/main/GameInit_L_or_D_Test.java
@@ -0,0 +1,13 @@
+package pij.main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameInit_L_or_D_Test {
+
+    @Test
+    void userInputDLoadsDirectory() {
+
+    }
+}
