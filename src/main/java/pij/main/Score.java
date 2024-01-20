7b0a0c0ceb535ac74ee0b97191ec40114273e12f
Ricki Angel <rangel03@student.bbk.ac.uk>
2024-01-20 16:56:00 +0000

    intiially Created a Score class to read tile letter value

--- src/main/java/pij/main/Score.java
+++ src/main/java/pij/main/Score.java
@@ -2,6 +2,9 @@ package pij.main;
 /**
 This class contains all methods and code for keeping score for
 Human and computer scores.
+
 */
 public class Score {
+// TODO Write a HashMap that reads the tiles letter value.}
+
 }
